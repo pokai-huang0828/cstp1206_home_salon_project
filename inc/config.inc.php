eba80b71367c475ea1ca66cc348eecddee2fded2
Eric Cheung <000437017@student.vcc.ca>
2020-11-25 20:58:56 -0800

    changed sql script and config

--- inc/config.inc.php
+++ inc/config.inc.php
@@ -3,4 +3,10 @@
 define("STYLIST_DATA", "data/stylist.data.csv");
 define("USER_DATA", "data/user.data.csv");
 
+// Database configuration properties: change accordingly
+define("DB_HOST", __HOSTNAME__);
+define("DB_USER", __USERNAME__);
+define("DB_PASS", __PASSWORD__);
+define("DB_NAME", "homesalonapp");
+
 ?>
