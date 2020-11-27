8f751535ab5fc6b40d9f8ca0be76d81e31acb12c
Eric Cheung <000437017@student.vcc.ca>
2020-11-27 15:59:18 -0800

    change config str

--- inc/config.inc.php
+++ inc/config.inc.php
@@ -4,9 +4,14 @@ define("STYLIST_DATA", "data/stylist.data.csv");
 define("USER_DATA", "data/user.data.csv");
 
 // Database configuration properties: change accordingly
-define("DB_HOST", __HOSTNAME__);
-define("DB_USER", __USERNAME__);
-define("DB_PASS", __PASSWORD__);
+define("DB_HOST", __Host__);
+define("DB_USER", __User__);
+define("DB_PASS", __Password__);
 define("DB_NAME", "homesalonapp");
+
+// define("DB_HOST", "localhost");
+// define("DB_USER", "root");
+// define("DB_PASS", "123456");
+// define("DB_NAME", "homesalonapp");
 
 ?>
